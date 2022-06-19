3e15a26c2fa63d01e5f7bb73bc8280ce798ff8bb
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-06-19 14:17:22 +0530

    initial setup & file structure

---
+++ components/Balance.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Balance = () => {
+  return (
+    <div className=''>Balance</div>
+  )
+}
+
+export default Balance
---
+++ components/ExploreCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExploreCard = () => {
+  return (
+    <div>ExploreCard</div>
+  )
+}
+
+export default ExploreCard
